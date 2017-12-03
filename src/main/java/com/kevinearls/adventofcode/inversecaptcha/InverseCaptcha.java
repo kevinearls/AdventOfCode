50230efdfdf8382d10c797c5826355a6c06ca6ee
Kevin Earls <kearls@redhat.com>
2017-12-03 12:30:39 +0100

    Updates for part 2 of day1

--- src/main/java/com/kevinearls/adventofcode/inversecaptcha/InverseCaptcha.java
+++ src/main/java/com/kevinearls/adventofcode/inversecaptcha/InverseCaptcha.java
@@ -1,43 +1,37 @@
 package com.kevinearls.adventofcode.inversecaptcha;
 /**
- * The captcha requires you to review a sequence of digits (your puzzle input) and find the sum of all digits that match
- * the next digit in the list. The list is circular, so the digit after the last digit is the first digit in the list.
+ * You notice a progress bar that jumps to 50% completion. Apparently, the door isn't yet satisfied, but it did emit a
+ * star as encouragement. The instructions change: Now, instead of considering the next digit, it wants you to consider
+ * the digit halfway around the circular list. That is, if your list contains 10 items, only include a digit in your sum
+ * if the digit 10/2 = 5 steps forward matches it. Fortunately, your list has an even number of elements.
 
  For example:
 
- 1122 produces a sum of 3 (1 + 2) because the first digit (1) matches the second digit and the third digit (2) matches the fourth digit.
- 1111 produces 4 because each digit (all 1) matches the next.
- 1234 produces 0 because no digit matches the next.
- 91212129 produces 9 because the only digit that matches the next one is the last digit, 9.
- What is the solution to your captcha?
+ 1212 produces 6: the list contains 4 items, and all four digits match the digit 2 items ahead.
+ 1221 produces 0, because every comparison is between a 1 and a 2.
+ 123425 produces 4, because both 2s match each other, but no other digit has a match.
+ 123123 produces 12.
+ 12131415 produces 4.
+ What is the solution to your new captcha?
 
  */
 public class InverseCaptcha {
     public int sum(String input) {
-        // TODO check input?
+        // TODO check input?  Needs to be of even length and all digits
 
         int[] contents = new int[input.length()];
         for (int i=0; i < input.length(); i++) {
-            //if input.charAt(i)
             contents[i] = input.charAt(i) - '0';
         }
 
         int total = 0;
-        for (int i=0; i < (contents.length -1); i++) {
-            if (contents[i] == contents[i + 1]) {
-                total += contents[i];
+        int lookahead = contents.length / 2;
+        for (int i=0; i < (contents.length / 2); i++) {
+            if (contents[i] == contents[i + lookahead]) {
+                total += contents[i] + contents[i + lookahead];
             }
         }
-        // Special case, compare last to first
-        if (contents[0] == contents[contents.length-1]) {
-            total += contents[0];
-        }
+
         return total;
     }
-
-    public static void main(String[] args) {
-        InverseCaptcha day1 = new InverseCaptcha();
-        System.out.println("1122: " + day1.sum("1122"));
-    }
-
 }
